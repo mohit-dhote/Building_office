1d33645a1a84cf86556864d3ba29d501fa407ab0
Komalggulhane <komalgulhane007@gmail.com>
2023-08-12 11:07:48 +0530

    I have added some changes

---
+++ komal.c
@@ -0,0 +1,13 @@
+
+int main() {  
+    char c;
+    printf("Enter a string: ");
+    scanf("%c", &c);  
+    
+    // %d displays the integer value of a character
+    // %c displays the actual character
+    printf("ASCII value of %c = %d", c, c);
+    
+    return 0;
+}
+This is the new file
